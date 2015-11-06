2ab2c2417549b1b894ed8493509c51d4b7c8fb76
Chip Self <chipself9@gmail.com>
2015-11-05 21:44:11 -0500

    final refactor

--- queen_attack.rb
+++ queen_attack.rb
@@ -1,42 +1,84 @@
 class Queens
-  attr_accessor :white, :black, :board_grid
-  DIMENSION = 7
+  attr_accessor :white, :black, :board
+  MAXDIMENSION = 7
 
   def initialize(white: [0,3], black: [7,3])
     if white == black
-      raise ArgumentError.new("Both Queens cannot be in the same position")
+      raise ArgumentError.new("Both Queens cannot be in the same position.")
     else
       @white = white
       @black = black
+      @board = ""
     end
   end
 
   def to_s
-    board_grid = ""
-    (0..DIMENSION).each do |y|
-      (0..DIMENSION).each do |x|
-        if y == @white[0] && x == @white[1]
-          board_grid = board_grid + "W "
-        elsif y == @black[0] && x == @black[1]
-          board_grid = board_grid + "B "
-        elsif x == DIMENSION && y != DIMENSION
-          board_grid = board_grid + "_\n"
-        elsif x == DIMENSION && y == DIMENSION
-          board_grid = board_grid + "_"
-        else
-          board_grid = board_grid + "_ "
-        end
+    (0..MAXDIMENSION).each do |y|
+      (0..MAXDIMENSION).each do |x|
+        create_board(y, x)
       end
     end
-    @board_grid = board_grid
+    @board
   end
 
   def attack?
-    return true if (@white[0] == @black[0]) || (@white[1] == @black[1])
-    return true if (@white[0] +  @black[0]) == (@white[1] +  @black[1])
-    return true if (@white[0] +  @black[1]) == (@white[1] +  @black[0])
-    return false
+    case attack_type
+    when :row, :column, :diagonal
+      true
+    when :none
+      false
+    end
   end
 
 
+
+
+  private
+
+  def create_board(y, x)
+    case evaluate_position(y, x)
+    when :white
+      place_char("W ")
+    when :black
+      place_char("B ")
+    when :new_line
+      place_char("_\n")
+    when :underscore
+      place_char("_")
+    when :space
+      place_char("_ ")
+    end
+  end
+
+  def evaluate_position(y, x)
+    if y == @white[0] && x == @white[1]
+      :white
+    elsif y == @black[0] && x == @black[1]
+      :black
+    elsif x == MAXDIMENSION && y != MAXDIMENSION
+      :new_line
+    elsif x == MAXDIMENSION && y == MAXDIMENSION
+      :underscore
+    else
+      :space
+    end
+  end
+
+  def place_char(char)
+    @board = @board + char
+  end
+
+  def attack_type
+    if (@white[0] == @black[0])
+      type = :row
+    elsif (@white[1] == @black[1])
+      type = :column
+    elsif (@white[0] +  @black[0]) == (@white[1] +  @black[1])
+      type = :diagonal
+    elsif (@white[0] +  @black[1]) == (@white[1] +  @black[0])
+      type = :diagonal
+    else
+      type = :none
+    end
+  end
 end
